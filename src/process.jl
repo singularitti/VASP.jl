925a3e779b6e13d7e6380dde22369bbda0e8e10a
singularitti <singularitti@outlook.com>
2026-02-15 17:08:34 -0600

    Implement `list_files` in src/process.jl

--- src/process.jl
+++ src/process.jl
@@ -1,11 +1,17 @@
-using FileTrees: FileTree, filter, path
+using FileTrees: FileTree, filter, path, name
 
-export walk_workdirs
+export walk_workdirs, list_files
 
 function walk_workdirs(root)
     tree = FileTree(root)
     return filter(tree; dirs=true) do node
         # Must compare the node to the tree itself, otherwise we will exclude the root directory
         node isa FileTree ? (node == tree || isvalid(WorkDir(path(node)))) : true
+    end
+end
+
+function list_files(tree::FileTree, pattern)
+    return filter(tree; dirs=false) do node
+        node isa FileTree ? false : pattern == name(node)
     end
 end
