2f4d82a9e75e2ccc1bd86d79d14b804fcb5177e8
singularitti <singularitti@outlook.com>
2026-02-15 13:31:38 -0600

    Loose criteria for comparison in `walk_workdirs`

--- src/process.jl
+++ src/process.jl
@@ -6,6 +6,6 @@ function walk_workdirs(root)
     tree = FileTree(root)
     return filter(tree; dirs=true) do node
         # Must compare the node to the tree itself, otherwise we will exclude the root directory
-        node isa FileTree ? (node === tree || isvalid(WorkDir(path(node)))) : true
+        node isa FileTree ? (node == tree || isvalid(WorkDir(path(node)))) : true
     end
 end
