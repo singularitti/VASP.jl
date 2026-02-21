5f3e6b87c42b7d02622abb22f7a90d4d088b8b1a
singularitti <singularitti@outlook.com>
2026-02-20 23:40:23 -0600

    Fix loading order of Indexing.jl

--- src/VASP.jl
+++ src/VASP.jl
@@ -4,9 +4,9 @@ using OrderedCollections: LittleDict
 
 include("python.jl")
 include("WorkDir.jl")
+include("Indexing.jl")
 include("process.jl")
 include("WorkStatus.jl")
-include("Indexing.jl")
 include("Poscar.jl")
 include("Potcar.jl")
 include("magnetization.jl")
